9e86e086cff4ad6aecde57cab33ada02a5b058c2
Osvaldo Aurélio <osvaldo@viasoft.com.br>
2021-02-20 12:06:25 -0300

    Ajust Procfile

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,9 @@ module.exports = {
   username: process.env.USER_DB,
   password: process.env.PASS_DB,
   database: process.env.NAME_DB,
+  ssl: {
+    rejectUnauthorized: false
+  },
   define: {
     timestamps: true,
     underscored: true,

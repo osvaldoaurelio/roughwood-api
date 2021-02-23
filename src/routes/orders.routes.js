68f7dc08a660134bbf19b04ff462655ea2bf1c83
Osvaldo Aurélio <osvaldo@viasoft.com.br>
2021-02-22 23:31:04 -0300

    adding routes done and progress

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -12,6 +12,8 @@ ordersRouter.post('/', ensureAuthenticatedAdmin, OrdersController.store);
 ordersRouter.get('/:id', ensureAuthenticatedAdmin, OrdersController.show);
 ordersRouter.put('/:id', ensureAuthenticatedAdmin, OrdersController.edit);
 ordersRouter.put('/:id/invoice', ensureAuthenticatedAdmin, OrdersController.invoice);
+ordersRouter.put('/:id/done', ensureAuthenticatedUser, OrdersController.done);
+ordersRouter.put('/:id/progress', ensureAuthenticatedUser, OrdersController.progress);
 ordersRouter.delete('/:id', ensureAuthenticatedAdmin, OrdersController.destroy);
 
 module.exports = ordersRouter;
